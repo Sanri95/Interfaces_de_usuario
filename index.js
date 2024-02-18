4acac8822503b4a333ddec32bb1141ff2999fcf4
Sanri95 <sanrio95@gmail.com>
2024-02-18 15:02:17 -0500

    Crear funcion para la imagen cambie

--- index.js
+++ index.js
@@ -8,3 +8,8 @@ btn.addEventListener('click', ()=>{
     btn.style.backgroundColor ='#ffac09';
     btn.style.boxshadow ='0 0 10px #ffac09'; 
 });
+
+//funcion para cambiar la imagen cuando pase el ursor por encima
+// se inicia un evento de escuchar con los parametros load iniciar el false para que cuando pase
+// el cursor este listo usarla funcion iniciar 
+window.addEventListener('load', iniciar, false);
