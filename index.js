641359f5c49b2f40fa42fca75e2b0e4a4d896787
Sanri95 <sanrio95@gmail.com>
2024-02-18 17:38:24 -0500

    Crear funcion para cambia imagen

--- index.js
+++ index.js
@@ -13,3 +13,17 @@ btn.addEventListener('click', ()=>{
 // se inicia un evento de escuchar con los parametros load iniciar el false para que cuando pase
 // el cursor este listo usarla funcion iniciar 
 window.addEventListener('load', iniciar, false);
+
+// iniciar img lo guarda en una variable llamada img (que es lo que esta guardado en el id img)
+// despues mediante el listener  al  mouse pasar por encima usa la funcion peligro 
+function iniciar() {
+    var imagen = document.getElementById('img');
+    imagen.addEventListener('mouseover', peligro, false);
+  }
+  
+  // peligro toma la variable imagen que lo que esta guardado en el id img y cambia el valor por lo que este en 
+  // src (en este caso la imagen guardada en la carpeta img)
+  function peligro() {
+    var imagen = document.getElementById('img').src = "img/img2.avif";
+  }
+    
