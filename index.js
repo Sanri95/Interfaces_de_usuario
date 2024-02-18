e1bb2d3e020b76912dc636ae5896ae07fce93746
Sanri95 <sanrio95@gmail.com>
2024-02-18 15:00:29 -0500

    Crear funcion para que el boton cambie de color

--- index.js
+++ index.js
@@ -3,3 +3,8 @@
 // se  declara la constante que corresponde al ID .btn
 const btn = document.querySelector('.btn');
 
+// se usa la funcion de escuchar para cuando se de clic cambie el estilo del boton
+btn.addEventListener('click', ()=>{
+    btn.style.backgroundColor ='#ffac09';
+    btn.style.boxshadow ='0 0 10px #ffac09'; 
+});
