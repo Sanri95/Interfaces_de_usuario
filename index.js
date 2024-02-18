da0e9f6161a7f2c79495b434a33db3da0c6bca87
PatinoYe <yepatinop@gmail.com>
2024-02-18 18:29:28 -0500

    Revision del codigo

--- index.js
+++ index.js
@@ -3,11 +3,13 @@
 // se  declara la constante que corresponde al ID .btn
 const btn = document.querySelector('.btn');
 
+
 // se usa la funcion de escuchar para cuando se de clic cambie el estilo del boton
 btn.addEventListener('click', ()=>{
     btn.style.backgroundColor ='#ffac09';
     btn.style.boxshadow ='0 0 10px #ffac09'; 
 });
+
 
 //funcion para cambiar la imagen cuando pase el ursor por encima
 // se inicia un evento de escuchar con los parametros load iniciar el false para que cuando pase
@@ -17,13 +19,13 @@ window.addEventListener('load', iniciar, false);
 // iniciar img lo guarda en una variable llamada img (que es lo que esta guardado en el id img)
 // despues mediante el listener  al  mouse pasar por encima usa la funcion peligro 
 function iniciar() {
-    var imagen = document.getElementById('img');
-    imagen.addEventListener('mouseover', peligro, false);
-  }
-  
-  // peligro toma la variable imagen que lo que esta guardado en el id img y cambia el valor por lo que este en 
-  // src (en este caso la imagen guardada en la carpeta img)
-  function peligro() {
-    var imagen = document.getElementById('img').src = "img/img2.avif";
-  }
-    
+  var imagen = document.getElementById('img');
+  imagen.addEventListener('mouseover', peligro, false);
+}
+
+// peligro toma la variable imagen que lo que esta guardado en el id img y cambia el valor por lo que este en 
+// src (en este caso la imagen guardada en la carpeta img)
+function peligro() {
+  var imagen = document.getElementById('img').src = "img/img2.avif";
+}
+
